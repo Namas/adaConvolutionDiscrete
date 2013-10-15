45c7a3785f55076c3b13a3a72a1af52ad1accaea
namas <gostmasterys@gmail.com>
2013-10-15 17:12:14 +0200

    implémentation algo convolution

--- sources/Convolution.adb
+++ sources/Convolution.adb
@@ -24,7 +24,44 @@ procedure Convolution is
    -------------------------------------------
    -----------Fonctions et procédures---------
    -------------------------------------------
-  
+
+   --contiendra les données en niveau de gris d'une image
+   --la première dimmension décrit le parcourt de haut en bas de l'image
+   --la deuxième, le parcourt de gauche à droite
+   --càd: tab(0,0) = pixel en haut à gauche et tab(max,max) en bas à droite 
+   type T_Image is array(Integer range <>, Integer range <>) of Integer;
+
+   function convolutionDiscrete(t:T_Image) return T_Image is
+      i:Integer:=0;
+      k:Integer:=0;
+      tabC:T_Image(t'range(1),t'range(2)):=(others=>(others=>0));
+   begin -- convolutionDiscrete
+
+      -- opération sur les quatres coins de l'image
+      tabC(tabC'First(1),tabC'First(2)) = 2*t(t'First(1),t'First(2))-t(t'First(1)+1,t'First(2))-t(t'First(1),t'First(2)+1);
+      tabC(tabC'Last(1),tabC'First(2)) = 2*t(t'Last(1),t'First(2))-t(t'Last(1)-1,t'First(2))-t(t'Last(1),t'First(2)+1);
+      tabC(tabC'First(1),tabC'Last(2)) = 2*t(t'First(1),t'Last(2))-t(t'First(1)+1,t'Last(2))-t(t'First(1),t'Last(2)-1);
+      tabC(tabC'Last(1),tabC'Last(2)) = 2*t(t'Last(1),t'Last(2))-t(t'Last(1)-1,t'Last(2))-t(t'Last(1),t'Last(2)-1);
+
+      --sur la rangée du haut et celle du bas
+      for i in t'First(2)+1..t'Last(2)-1 loop
+         tabC(tabC'First(1),i) = 3*t(t'First(1),i)-t(t'First(1)+1,i)-t(t'First(1),i+1)-t(t'First(1),i-1);
+         tabC(tabC'Last(1),i) = 3*t(t'Last(1),i)-t(t'Last(1)-1,i)-t(t'Last(1),i+1)-t(t'Last(1),i-1);
+      end loop;
+
+      --sur la rangée de droite et celle de gauche
+      for i in t'First(1)+1..t'Last(1)-1 loop
+         tabC(i,tabC'First(2))=3*t(i,t'First(2))-t(i,t'First(2)+1)-t(i+1,t'First(2))-t(i-1,t'First(2));
+         tabC(i,tabC'Last(2))=3*t(i,t'Last(2))-t(i,t'Last(2)-1)-t(i+1,t'Last(2))-t(i-1,t'Last(2));
+      end loop;
+
+      --sur le centre de l'image
+      for i in t'First(1)+1..t'Last(1)-1 loop
+         for k in t'First(2)+1..t'Last(2)-1 loop
+            tabC(i,k)=4*t(i,k)-t(i,k+1)-t(i,k-1)-t(i+1,k)-t(i-1,k);
+         end loop;
+      end loop;
+   end convolutionDiscrete;
   
 
 
