d2d16df9818a059e661e30c63d97cf2ba75c9a0d
jonataubert <jonataubert@hotmail.com>
2013-10-15 14:39:49 +0200

    First source file

---
+++ sources/Convolution.adb
@@ -0,0 +1,42 @@
+------------------------------------------------------------
+--
+-- Author : Jonathan Aubert & Thierry Meister
+-- Description : 
+-- Ameliorations :
+------------------------------------------------------------
+
+with Text_IO; use Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
+with Ada.Numerics.Generic_Elementary_Functions;
+
+procedure Convolution is
+   -------------------------------------------
+   -----------Déclaration Variables-----------
+   -------------------------------------------
+
+   
+   
+   package Math is new Ada.Numerics.Generic_Elementary_Functions(Float);
+   USE Math;
+
+   -------------------------------------------
+   -----------Fonctions et procédures---------
+   -------------------------------------------
+  
+  
+
+
+------------------------------------------------------------
+-- START OF MAIN PROGRAM
+------------------------------------------------------------
+
+
+BEGIN   --Convolution
+
+
+
+END Convolution;
+
+
